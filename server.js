ed03e68513214d7a30e3f847bf7fbd2c66935be2
NemSothea <nemsothea13@gmail.com>
2017-10-21 20:10:09 -0700

    first init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  console.log(`${now}:${req.method}:${req.url}`);
+next();
+});
+app.get('/',(req, res)=>{
+  res.render('home.hbs',{
+    titlePage: 'Home page',
+    currentYear: new Date().getFullYear()
+  });
+});
+app.get('/about', (req, res) =>{
+  console.log('here');
+  res.render('about.hbs',{
+    titlePage: 'About page'
+  });
+});
+
+app.listen(3000,()=>{
+  console.log('Server run on port 3000');
+});
