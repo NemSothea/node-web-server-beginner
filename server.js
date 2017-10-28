876d93274d95679b3ae49ec4078d69fea8fa2c64
NemSothe <nemsothea13@gmail.com>
2017-10-28 23:07:07 +0700

    script on port 3000 and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 var app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine','hbs');
@@ -26,6 +27,6 @@ app.get('/about', (req, res) =>{
   });
 });
 
-app.listen(3000,()=>{
-  console.log('Server run on port 3000');
+app.listen(port,()=>{
+  console.log(`Server run on ${port}`);
 });
